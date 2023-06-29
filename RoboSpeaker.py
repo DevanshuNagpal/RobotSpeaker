e237d7ae77584da4c8dcc17f029723365d476641
DevanshuNagpal <133241751+DevanshuNagpal@users.noreply.github.com>
2023-06-30 05:22:49 +0530

    Update RoboSpeaker.py

--- RoboSpeaker.py
+++ RoboSpeaker.py
@@ -9,3 +9,16 @@ if __name__ == '__main__':
             break
         command =f'mshta vbscript:Execute("CreateObject(""SAPI.SpVoice"").Speak(""{x}"")(window.close)")'
         os.system(command) #automatically runs the command
+'''
+If you want to use Powershell instead of command promt use
+        command = f'PowerShell -Command "Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\\"{x}\\");"'
+
+If you are not using windows os-
+
+    For mac update the command to
+         command = f'say "{x}"'
+
+    For ubuntu
+        command = f'spd-say "{x}"'
+
+'''
